f39805e49d2f39fa78c02c330264128d538696ea
Shao Cheng <astrohavoc@gmail.com>
2016-05-09 00:03:33 +0800

    Implement concurrent wrapper for downloading a chunk

--- src/Web/Baidu/PCS.hs
+++ src/Web/Baidu/PCS.hs
@@ -1,14 +1,16 @@
 module Web.Baidu.PCS where
 
 import ClassyPrelude.Conduit
+import Control.Concurrent.Async
 import Control.Concurrent.SSem
 import Crypto.Hash
 import Crypto.Hash.Conduit
 import Crypto.Hash.Types
 import Data.ByteString.Base16
-import Network.HTTP.Client
+import Network.HTTP.Client hiding (fileSize)
 import Network.HTTP.Client.TLS
 import Network.HTTP.Types
+import System.IO (hSeek, IOMode(WriteMode), SeekMode(AbsoluteSeek), withBinaryFile)
 
 data GlobalConfig = GlobalConfig {
     accessToken :: !ByteString,
@@ -38,7 +40,8 @@ data FileSession = FileSession {
     globalSession :: !GlobalSession,
     fileDefaultRequest :: !Request,
     fileConfig :: !FileConfig,
-    fileHandle :: Handle
+    fileHandle :: !Handle,
+    progressMeter :: !(IORef Int)
 }
 
 initGlobalSession :: GlobalConfig -> IO GlobalSession
@@ -55,23 +58,6 @@ initGlobalSession cfg = do
         retryMeter = rm,
         progressLock = pl,
         connSem = cs
-    }
-
-initFileSession :: GlobalSession -> FileConfig -> IO FileSession
-initFileSession gs fc = do
-    let tok = (accessToken . config) gs
-    req <- parseUrl "https://d.pcs.baidu.com/rest/2.0/pcs/file"
-    let req' = req {
-        method = methodGet,
-        requestHeaders = [(hAccept,"*/*"),(hUserAgent,"netdisk;5.4.1.0;PC;PC-Windows;10.0.10586;WindowsBaiduYunGuanJia")],
-        queryString = renderQuery True [("method",Just "download"),("access_token",Just tok),("path",Just $ encodeUtf8 $ fileURL fc)],
-        cookieJar = Nothing,
-        checkStatus = \_ _ _ -> Nothing
-    }
-    pure FileSession {
-        globalSession = gs,
-        fileDefaultRequest = req',
-        fileConfig = fc
     }
 
 -- Here we use [L,R), different from [L,R] in the original API
@@ -93,11 +79,37 @@ downloadChunk FileSession {..} l r = go
             flag <- atomicModifyIORef' (retryMeter globalSession) $ \m -> if m == 0 then (0,False) else (m-1,True)
             if flag then go else fail s
 
-makeProgress :: FileSession -> Int -> Int -> IO ()
-makeProgress = error "todo"
+makeProgress :: FileSession -> Int -> Int -> Concurrently ()
+makeProgress fs@FileSession {..} l r = Concurrently $ do
+    chunk <- withSem (connSem globalSession) $ downloadChunk fs l r
+    withMVar (progressLock globalSession) $ const $ do
+        hSeek fileHandle AbsoluteSeek $ fromIntegral l
+        hPut fileHandle chunk
+        modifyIORef' progressMeter $ \m -> m+r-l
+        m <- readIORef progressMeter
+        putStr $ fileURL fileConfig ++ ": " ++ tshow m ++ " / " ++ tshow (fileSize fileConfig)
 
-downloadFile :: FileSession -> IO ()
-downloadFile = error "todo"
+downloadFile :: GlobalSession -> FileConfig -> IO ()
+downloadFile gs fc = do
+    let tok = (accessToken . config) gs
+    req <- parseUrl "https://d.pcs.baidu.com/rest/2.0/pcs/file"
+    let req' = req {
+        method = methodGet,
+        requestHeaders = [(hAccept,"*/*"),(hUserAgent,"netdisk;5.4.1.0;PC;PC-Windows;10.0.10586;WindowsBaiduYunGuanJia")],
+        queryString = renderQuery True [("method",Just "download"),("access_token",Just tok),("path",Just $ encodeUtf8 $ fileURL fc)],
+        cookieJar = Nothing,
+        checkStatus = \_ _ _ -> Nothing
+    }
+    pm <- newIORef 0
+    withBinaryFile (filePath fc) WriteMode $ \hdl -> do
+        let fs = FileSession {
+            globalSession = gs,
+            fileDefaultRequest = req',
+            fileConfig = fc,
+            fileHandle = hdl,
+            progressMeter = pm
+        }
+        error "todo"
 
 verifyMD5 :: FilePath -> ByteString -> IO Bool
 verifyMD5 path md5 = do
